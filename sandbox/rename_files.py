408a7b2c39a128d05855a28496dec1682c84172c
Katy Scott <k.l.scott16@gmail.com>
2025-05-05 19:03:47 +0000

    feat: added roi_name variable for directory path customization

--- sandbox/rename_files.py
+++ sandbox/rename_files.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import re
 
 proc_data_dir = Path("../data/procdata")
-dataset_name = "HEAD-NECK-RADIOMICS-HN1"
+dataset_name = "NSCLC-Radiomics"
 dataset_source = "TCIA"
+roi_name = 'GTV'
 
 path_to_files = proc_data_dir / (dataset_source + "_" + dataset_name) / 'pyradiomics_original_all_features'
 
@@ -12,6 +13,6 @@ for file in path_to_files.rglob('*.csv'):
     pattern = dataset_name + r"_\d{3}_"
     new_filename = re.sub(pattern, '', file.name)
     
-    if not file.parent.name == 'GTV-1':
-        (file.parent / "GTV-1").mkdir(parents=True, exist_ok=True) 
-        file.rename(file.parent / "GTV-1" / new_filename)
+    if not file.parent.name == roi_name:
+        (file.parent / roi_name).mkdir(parents=True, exist_ok=True) 
+        file.rename(file.parent / roi_name / new_filename)
