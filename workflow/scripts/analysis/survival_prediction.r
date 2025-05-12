a11f54478b503fda0ab7102a6b3df3dce3a93291
Katy Scott <k.l.scott16@gmail.com>
2025-05-12 21:17:37 +0000

    feat: start script for R concordance index calculations

---
+++ workflow/scripts/analysis/survival_prediction.r
@@ -0,0 +1,7 @@
+DATA_DIR_PATH <- Path("../../data")
+PROC_DATA_PATH <- DATA_DIR_PATH / "procdata" / DATASET_NAME
+RESULTS_DATA_PATH <- DATA_DIR_PATH / "results" / DATASET_NAME
+
+signature_name <- "aerts_original"
+
+config_file <- paste0()
