638df2bc64da38d3823b7bd1058eb2b68dddf871
strixy16 <k.l.scott16@gmail.com>
2025-07-23 19:42:43 +0000

    feat: add docstrings and functionality for train test splitting

--- workflow/scripts/analysis/python/predict.py
+++ workflow/scripts/analysis/python/predict.py
@@ -7,7 +7,6 @@ import click
 from joblib import Parallel, delayed
 from readii.utils import logger
 from readii.io.loaders import loadImageDatasetConfig, loadFileToDataFrame
-from readii.process.config import get_full_data_name
 from readii.process.label import (
     eventOutcomeColumnSetup,
     getPatientIdentifierLabel,
@@ -61,16 +60,17 @@ def load_signature_config(file: str | Path) -> pd.Series:
 
 def prediction_data_splitting(dataset_config,
                               data : pd.DataFrame,
-                              train_variable : str = 'training',
-                              test_variable : str = 'test'
                               ) -> tuple[pd.DataFrame]:
     """Split metadata into train and test for model development and validation purposes"""
     split_settings = dataset_config['ANALYSIS']['TRAIN_TEST_SPLIT']
+    # Construct the dictionary input expected to describe the split column and labels within it
+    split_col_dict = {split_settings['split_variable']: [split_settings['train_label'], split_settings['test_label']]}
+    # Check that split setting is true before completing split
     if split_settings['split']:
         split_data = splitDataByColumnValue(data,
-                                            split_col_data=split_settings['split_variable'],
+                                            split_col_data=split_col_dict,
                                             impute_value=split_settings['impute'])
-        return split_data[train_variable], split_data[test_variable]
+        return split_data[split_settings['train_label']], split_data[split_settings['test_label']]
     else:
         logger.debug('Split setting is set to False. Returning original data.')
         return data
@@ -109,7 +109,8 @@ def insert_mit_index(dataset_config: str,
 
 
 def clinical_data_setup(dataset_config,
-                       full_dataset_name : str | None = None
+                       full_dataset_name : str | None = None,
+                       split: str | None = None
                        ) -> pd.DataFrame:
     """Process the clinical data to get outcome variables for use in signature prediction"""
     if full_dataset_name is None:
@@ -130,6 +131,18 @@ def clinical_data_setup(dataset_config,
     if len(clinical['EXCLUSION_VARIABLES']) != 0:
         clinical_data = selectByColumnValue(clinical_data,
                                             exclude_col_values = clinical['EXCLUSION_VARIABLES'])
+
+    # Get the train or test sub-cohort based on config file setup
+    if split is not None:
+        split_info = dataset_config['ANALYSIS']['TRAIN_TEST_SPLIT']
+        match split:
+            case 'TRAIN':
+                keep_data = {split_info['split_variable']: [split_info['train_label']]}
+            case 'TEST':
+                keep_data = {split_info['split_variable']: [split_info['train_label']]}
+        
+        clinical_data = selectByColumnValue(clinical_data,
+                                            include_col_values=keep_data)
 
     return clinical_data
 
@@ -162,6 +175,7 @@ def outcome_data_setup(dataset_config,
     return outcome_data
 
 
+
 def calculate_signature_hazards(feature_data : pd.DataFrame,
                                 signature : pd.DataFrame) -> pd.DataFrame:
     """Calculate the feature hazards with Cox Proportional Hazards by multiplying feature values by the signature weights"""
@@ -172,7 +186,9 @@ def calculate_signature_hazards(feature_data : pd.DataFrame,
     return signature_feature_data.dot(signature)
 
 
+
 def evaluate_signature_prediction(hazards_and_outcomes : pd.DataFrame) -> tuple:
+    """Get the c-index for a set of predictions/hazards."""
     concordance_evals = concordance_index_censored(
             event_indicator = hazards_and_outcomes['survival_event_binary'].astype(bool),
             event_time = hazards_and_outcomes['survival_time_years'],
@@ -183,9 +199,11 @@ def evaluate_signature_prediction(hazards_and_outcomes : pd.DataFrame) -> tuple:
     return concordance_evals[0]
 
 
+
 def bootstrap_c_index(hazards_and_outcomes: pd.DataFrame,
                       bootstrap_count: int = 1000,             
                      ) -> tuple[list[float], float, float]:
+    """Generate confidence intervals by bootstrapping a set of prediction/hazard values by sampling with replacement and calculating metrics."""
     if bootstrap_count < 1:
         message = "Bootstrap count must be a positive integer."
         logger.error(message)
@@ -212,6 +230,7 @@ def predict_with_one_image_type(feature_data,
                                 signature_name : str,
                                 bootstrap : int = 0
                                 ) -> tuple[list[float], list[float], pd.DataFrame]:
+    """Evaluate the outcome prediction performance of a provided signature with a feature dataset from one image type. Optional bootstrapping for confidence intervals."""
     # load signature
     signature = load_signature_config(Path(f"{signature_name}.yaml"))
     # Set index in feature data to match outcome data
@@ -246,17 +265,20 @@ def predict_with_one_image_type(feature_data,
     return metrics, bootstrap_cidxs, hazards_and_outcomes
 
 
+DATA_SPLIT_CHOICES = ['TRAIN', 'TEST', 'NONE']
+
 @click.command()
 @click.option('--dataset', type=click.STRING, required=True, help='Name of the dataset to perform prediction with.')
 @click.option('--features', type=click.STRING, required=True, help='Feature type to load for prediction. Will match a feature extraction settings file in config.')
 @click.option('--signature', type=click.STRING, required=True, help='Name of the signature to perform prediction with. Must have file in config/signatures.')
-@click.option('--bootstrap', type=click.INT, required=False, help='Number of bootstrap iterations to run for confidence interval generation.')
+@click.option('--bootstrap', type=click.INT, default=0, help='Number of bootstrap iterations to run for confidence interval generation.')
+@click.option('--split', type=click.Choice(DATA_SPLIT_CHOICES), default='NONE', help="Data subset to use for prediction, TRAIN or TEST. Will get settings from dataset config.")
 def predict_with_signature(dataset: str,
                            features: str,
                            signature: str,
-                           bootstrap : int = 0,
-                           parallel: bool = False):
-    
+                           bootstrap: int = 0,
+                           split: str = 'NONE'):
+    """Run outcome prediction of a signature for multiple image types"""
     # Input checking
     if dataset is None:
         message = "Dataset name must be provided."
@@ -271,41 +293,33 @@ def predict_with_signature(dataset: str,
         message = "Signature name must be provided."
         logger.error(message)
         raise ValueError(message)
-
+    # Input checking
+    if split == 'NONE':
+        split = ""
 
     # get path to dataset config directory
     config_dir_path = dirs.CONFIG / 'datasets'
     
     # Load in dataset configuration settings from provided dataset name
     dataset_config = loadImageDatasetConfig(dataset, config_dir_path)
-    dataset_name = dataset_config['DATASET_NAME']
+    dataset_name = f"{dataset_config['DATASET_NAME']} {split}".strip()
     full_dataset_name = f"{dataset_config['DATA_SOURCE']}_{dataset_config['DATASET_NAME']}"
 
     logger.info(f"Performing prediction with {signature} signature on {dataset_name}.")
 
     # load clinical metadata
     clinical_data = clinical_data_setup(dataset_config, full_dataset_name)
-
-    # TODO: figure out what to do with train/test data
-    # if dataset_config['ANALYSIS']['TRAIN_TEST_SPLIT']['split']:
-    #     logger.info(f"Splitting data into train and test subsets.")
-    #     train_clinical, test_clinical = prediction_data_splitting(dataset_config, clinical_data)
-    #     train_outcome, test_outcome = outcome_data_setup(dataset_config, train_clinical), outcome_data_setup(dataset_config, test_clinical)
-    #     # Put into tuple for passing to prediction functions as a single argument
-    #     outcome_data = (train_outcome, test_outcome)
-    # else:
-
+    # get outcome variable data from clinical data
     outcome_data = outcome_data_setup(dataset_config, clinical_data)
 
     # Get image types from results of feature extraction
     image_type_feature_file_list = sorted(Path(dirs.RESULTS / full_dataset_name / "features").rglob(pattern = f"**/{features}/*_features.csv"))
 
     # Set up analysis outputs
-    prediction_out_dir = dirs.RESULTS / full_dataset_name / "prediction" / signature
+    prediction_out_dir = dirs.RESULTS / full_dataset_name / "prediction" / signature / split
+
     hazards_out_dir = prediction_out_dir / "hazards"
     hazards_out_dir.mkdir(parents=True, exist_ok=True)
-
-    
 
     prediction_data = []
     hazard_data = {}
@@ -316,10 +330,10 @@ def predict_with_signature(dataset: str,
         feature_data = loadFileToDataFrame(feature_file_path)
 
         prediction_metrics, bootstrap_cidx, hazards = predict_with_one_image_type(feature_data = feature_data,
-                                                               outcome_data = outcome_data,
-                                                               signature_name = signature,
-                                                               bootstrap = bootstrap)
-        
+                                                                                outcome_data = outcome_data,
+                                                                                signature_name = signature,
+                                                                                bootstrap = bootstrap)
+
         prediction_data = prediction_data + [[dataset_name, image_type] + prediction_metrics]
         hazard_data[image_type] = hazards
         bootstrap_data[image_type] = pd.DataFrame(data = bootstrap_cidx, columns=["C-index"])
@@ -342,6 +356,8 @@ def predict_with_signature(dataset: str,
         [bootstrap_df.to_csv(bootstrap_out_dir / f"{image_type}_c_idx.csv") for image_type, bootstrap_df in bootstrap_data.items()]
 
     return prediction_df, bootstrap_data, hazard_data
+
+
 
 if __name__ == "__main__":
     predict_with_signature()
