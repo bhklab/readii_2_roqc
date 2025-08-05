c599e8ff1c1677f4cfbbc5fe526db0d3103eb07a
strixy16 <k.l.scott16@gmail.com>
2025-08-05 21:08:20 +0000

    fix: correct function import name from loaders

--- src/readii_2_roqc/utils/__init__.py
+++ src/readii_2_roqc/utils/__init__.py
@@ -1,8 +1,8 @@
 from .metadata import get_masked_image_metadata, roi_filter_mask_metadata
-from .loaders import load_dataset_from_config
+from .loaders import load_dataset_config
 
 __all__ = [
     "roi_filter_mask_metadata",
     "get_masked_image_metadata",
-    "load_dataset_from_config"
+    "load_dataset_config"
 ]
