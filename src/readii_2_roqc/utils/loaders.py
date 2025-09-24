4e73a092eecd7a9f22aabd95657cf7223e6b4bbc
strixy16 <k.l.scott16@gmail.com>
2025-09-24 19:20:55 +0000

    feat: add load image and mask function to loaders to be used by make negative controls

--- src/readii_2_roqc/utils/loaders.py
+++ src/readii_2_roqc/utils/loaders.py
@@ -2,6 +2,10 @@ from damply import dirs
 from readii.io.loaders import loadImageDatasetConfig
 from readii.process.config import get_full_data_name
 from readii.utils import logger
+from readii.image_processing import alignImages, flattenImage
+
+import SimpleITK as sitk
+
 
 def load_dataset_config(dataset:str):
     if dataset is None:
@@ -19,3 +23,19 @@ def load_dataset_config(dataset:str):
     full_data_name = get_full_data_name(config_dir_path / dataset)
 
     return dataset_config, dataset_name, full_data_name
+
+
+
+def load_image_and_mask(image_path, mask_path = None):
+    # Load in image
+    raw_image = sitk.ReadImage(image_path)
+    # Remove extra dimension of image, set origin, spacing, direction to original
+    image = alignImages(raw_image, flattenImage(raw_image)) 
+
+    if mask_path:
+        # Load in mask
+        raw_mask = sitk.ReadImage(mask_path)
+        mask = alignImages(image, flattenImage(raw_mask))
+        return image, mask
+    
+    return image
