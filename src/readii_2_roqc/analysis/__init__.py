543c2e08ed1ef26a7d5c90a24d58391f72eed590
strixy16 <k.l.scott16@gmail.com>
2026-05-27 15:42:23 +0000

    refactor: update analysis init

--- src/readii_2_roqc/analysis/__init__.py
+++ src/readii_2_roqc/analysis/__init__.py
@@ -1,25 +1,19 @@
 from .predict import (
 	bootstrap_c_index,
+    bootstrap_auc,
 	calculate_signature_hazards,
-	clinical_data_setup,
 	evaluate_signature_prediction,
-	insert_r2r_index,
 	load_signature_config,
-	outcome_data_setup,
 	predict_with_one_image_type,
 	predict_with_signature,
-	prediction_data_splitting,
 )
 
 __all__ = [
 	"bootstrap_c_index",
+    "bootstrap_auc",
 	"calculate_signature_hazards",
-	"clinical_data_setup",
 	"evaluate_signature_prediction",
-	"insert_r2r_index",
 	"load_signature_config",
-	"outcome_data_setup",
 	"predict_with_one_image_type",
 	"predict_with_signature",
-	"prediction_data_splitting"
 ]
