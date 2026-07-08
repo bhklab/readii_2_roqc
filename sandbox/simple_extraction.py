2e0dbaac27f7677708d2923ea1ea049f12c30614
Katy Scott <k.l.scott16@gmail.com>
2026-07-08 14:44:50 -0400

    feat: setup cli for path inputs

--- sandbox/simple_extraction.py
+++ sandbox/simple_extraction.py
@@ -1,8 +1,10 @@
 from damply import dirs
+from pathlib import Path
 import pandas as pd
 from joblib import Parallel, delayed
 from tqdm import tqdm
 import yaml
+import click
 
 from readii_2_roqc.feature_extraction.extract import extract_sample_features, metadata_setup
 from readii_2_roqc.feature_extraction.index import get_mit_extraction_index
@@ -30,50 +32,76 @@ def extract_one_pyradiomics(
 
     return metadata
 
+@click.command()
+@click.argument('config_file', type=Path)
+@click.argument('nifti_dir', type=Path)
+@click.argument('feature_dir', type=Path)
+@click.argument('extract_settings', type=Path)
+@click.option('--n_jobs', default=-1, help='Number of parallel jobs to use')
+@click.option('--overwrite', is_flag=True, help='Overwrite existing feature files')
+def simple_extraction(
+    config_file: str,
+    nifti_dir: Path,
+    feature_dir: Path,
+    extract_settings: Path,
+    n_jobs: int = -1,
+    overwrite: bool = False
+)-> None:
+    """Extract features from a dataset using pyradiomics.
+    
+    CONFIG_FILE is the path to the dataset configuration file (YAML format).
 
+    NIFTI_DIR is the path to the directory containing med-imagetools converted NIfTI images. Must contain an index-simple.csv file.
 
-dataset = "HEAD-NECK-RADIOMICS-HN1"
+    FEATURE_DIR is the path to the directory where extracted features will be saved.
 
-config_file_path = dirs.CONFIG / 'datasets' / f'{dataset}.yaml'
-nifti_images_dir_path = dirs.PROCDATA / dataset / "nifti_images" 
-output_features_path = dirs.PROCDATA / dataset / "features"
-extraction_settings = dirs.CONFIG / 'pyradiomics' / 'linear_all_images_features.yaml'
+    EXTRACT_SETTINGS is the path to the extraction settings file (YAML format). 
+    """
+    # Load dataset configuration
+    with config_file.open("r") as f:
+        dataset_config = yaml.safe_load(f)
 
-n_jobs = -1
+    # Set up and save out the index file to run feature extraction with
+    mit_simple_index_path = nifti_dir / f"{nifti_dir.stem}_index-simple.csv"
+    extraction_index = get_mit_extraction_index(dataset_config, mit_simple_index_path)
+    if 'DataSource' not in extraction_index.columns:
+        extraction_index['DataSource'] = 'PMCC'
 
-# Load dataset configuration
-with config_file_path.open("r") as f:
-    dataset_config = yaml.safe_load(f)
+    # Save out the extraction index to the output features path
+    feature_dir.mkdir(parents=True, exist_ok=True)
+    extraction_index.to_csv(feature_dir / f"pyradiomics_extraction_index.csv", index=False)
 
-# Set up and save out the index file to run feature extraction with
-mit_simple_index_path = nifti_images_dir_path / f"{nifti_images_dir_path.stem}_index-simple.csv"
-extraction_index = get_mit_extraction_index(dataset_config, mit_simple_index_path)
-if 'DataSource' not in extraction_index.columns:
-    extraction_index['DataSource'] = 'PMCC'
-
-output_features_path.mkdir(parents=True, exist_ok=True)
-extraction_index.to_csv(output_features_path / f"pyradiomics_{dataset}_index.csv", index=False)
-
-# initialize empty list to hold each feature set per sample
-feature_vector = []
-
-# Sequentially extract features
-feature_vectors = Parallel(n_jobs=n_jobs)(
-    delayed(extract_one_pyradiomics)(
-        sample_data=sample_data,
-        extraction_settings=extraction_settings,
-        nifti_images_dir_path=nifti_images_dir_path,
-        output_features_path=output_features_path,
-        overwrite=True
+    # extract features in parallel for each sample in the extraction index
+    feature_vectors = Parallel(n_jobs=n_jobs)(
+        delayed(extract_one_pyradiomics)(
+            sample_data=sample_data,
+            extraction_settings=extract_settings,
+            nifti_images_dir_path=nifti_dir,
+            output_features_path=feature_dir,
+            overwrite=overwrite
+        )
+        for _, sample_data in tqdm(
+            extraction_index.iterrows(),
+            desc=f"Extracting pyradiomics features",
+            total=len(extraction_index)
+        )
     )
-    for _, sample_data in tqdm(
-        extraction_index.iterrows(),
-        desc=f"Extracting pyradiomics features",
-        total=len(extraction_index)
-    )
-)
 
-# Concatenate all the feature vectors
-features_df = pd.DataFrame.from_dict(feature_vectors)
-# # Save out the features to a single CSV
-features_df.to_csv(output_features_path / f"pyradiomics_{dataset}_features.csv", index=False)
+    # Concatenate all the feature vectors
+    features_df = pd.DataFrame.from_dict(feature_vectors)
+    # # Save out the features to a single CSV
+    features_df.to_csv(feature_dir / f"pyradiomics_features.csv", index=False)
+
+
+if __name__ == "__main__":
+    simple_extraction()
+
+    # dataset = "HEAD-NECK-RADIOMICS-HN1"
+
+    # config_file_path = dirs.CONFIG / 'datasets' / f'{dataset}.yaml'
+    # nifti_images_dir_path = dirs.PROCDATA / dataset / "nifti_images" 
+    # output_features_path = dirs.PROCDATA / dataset / "features"
+    # extraction_settings = dirs.CONFIG / 'pyradiomics' / 'linear_all_images_features.yaml'
+
+    # n_jobs = -1
+    # overwrite = False
